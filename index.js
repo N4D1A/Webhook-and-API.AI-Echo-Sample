bda58aff7e86ff16ac6bcff1940b96fb44891d2d
N4D1A <35233687+N4D1A@users.noreply.github.com>
2018-05-20 22:22:20 +0900

    Update index.js

--- index.js
+++ index.js
@@ -5,27 +5,23 @@ const bodyParser = require("body-parser");
 
 const restService = express();
 
-restService.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-);
+restService.use(bodyParser.urlencoded({extended: true}));
 
 restService.use(bodyParser.json());
 
+/////// modified for v.2
 restService.post("/echo", function(req, res) {
   var speech =
-    req.body.result &&
-    req.body.result.parameters &&
-    req.body.result.parameters.echoText
-      ? req.body.result.parameters.echoText
+    req.body.queryResult &&
+    req.body.queryResult.parameters &&
+    req.body.queryResult.parameters.echoText
+      ? req.body.queryResult.parameters.echoText
       : "Seems like some problem. Speak again.";
   return res.json({
-    speech: speech,
-    displayText: speech,
-    source: "webhook-echo-sample"
+    fulfillmentText: speech,
   });
 });
+/////// 
 
 restService.post("/audio", function(req, res) {
   var speech = "";
